ec4198b1c0ae24532af3e60b3b3e35e15477e60b
Alexander Lindsay <aml.lindsay@gmail.com>
2016-03-18 22:16:00 -0700

    added ability to change the date by clicking on the date header

--- client/components/changedatecomponent.ts
+++ client/components/changedatecomponent.ts
@@ -4,8 +4,44 @@ import * as m from "mithril";
 import * as moment from "moment";
 import * as DF from "../utils/dateFormatter";
 
+class ChangeDateViewModel {
+
+    public date: _mithril.MithrilProperty<moment.Moment>;
+    public isEdit: _mithril.MithrilProperty<Boolean>;
+
+    constructor(public baseUrl: string, date: moment.Moment) {
+        this.date = m.prop(date);
+        this.isEdit = m.prop(false);
+    }
+
+    public setDate = (value: string) => {
+        let d = moment(value, DF.formats.pickerFormat);
+        if (d.isValid()) {
+            this.date(d);
+        }
+    };
+
+    public changeDate = () => {
+        const d = this.date();
+        if (d.isValid()) {
+            const url = this.dateToUrl(d);
+            this.isEdit(false);
+            m.route(url);
+        }
+    };
+
+    public dateToUrl = (date: moment.Moment) => {
+        return `${this.baseUrl}/${DF.formatDateForUrl(date)}`;
+    };
+}
+
 class ChangeDateController implements _mithril.MithrilController {
-    constructor(public date: moment.Moment) { }
+
+    public vm: ChangeDateViewModel;
+
+    constructor(baseUrl: string, date: moment.Moment) {
+        this.vm = new ChangeDateViewModel(baseUrl, date);
+    }
 }
 
 export default class ChangeDateComponent implements _mithril.MithrilComponent<ChangeDateController> {
@@ -14,7 +50,7 @@ export default class ChangeDateComponent implements _mithril.MithrilComponent<Ch
 
     constructor(baseUrl: string) {
         this.controller = (date) => {
-            return new ChangeDateController(date);
+            return new ChangeDateController(baseUrl, date);
         };
         this.view = (ctrl) => {
             return m("div.ui.segment",
@@ -22,21 +58,26 @@ export default class ChangeDateComponent implements _mithril.MithrilComponent<Ch
                     m("div.two.wide.column",
                         m("div.ui.basic.left.aligned.segment",
                             m("h2.ui.header",
-                                m("a", { href: `${baseUrl}/${DF.formatDateForUrl(ctrl.date.clone().subtract(1, "day"))}`, config: m.route }, m("i.arrow.left.icon"))
+                                m("a", { href: ctrl.vm.dateToUrl(ctrl.vm.date().clone().subtract(1, "day")), config: m.route }, m("i.arrow.left.icon"))
                             )
                         )
                     ),
                     m("div.twelve.wide.column",
                         m("div.ui.basic.center.aligned.segment",
-                            m("h2.ui.header",
-                                m("span", ctrl.date.format("dddd, MMMM Do YYYY"))
-                            )
+                            !ctrl.vm.isEdit() ?
+                                m("h2.ui.header",
+                                    m("a[href='#']", { onclick: ctrl.vm.isEdit.bind(null, true) }, DF.formatDateForDisplay(ctrl.vm.date()))
+                                ) :
+                                m("div.ui.action input", [
+                                    m("input[type='date']", { onchange: m.withAttr("value", ctrl.vm.setDate, null), value: DF.formatDateForPicker(ctrl.vm.date()) }),
+                                    m("button[type='button'].ui.button", { onclick: ctrl.vm.changeDate }, "Change Date")
+                                ])
                         )
                     ),
                     m("div.two.wide.column",
                         m("div.ui.basic.right.aligned.segment",
                             m("h2.ui.header",
-                                m("a", { href: `${baseUrl}/${DF.formatDateForUrl(ctrl.date.clone().add(1, "day"))}`, config: m.route }, m("i.arrow.right.icon"))
+                                m("a", { href: ctrl.vm.dateToUrl(ctrl.vm.date().clone().add(1, "day")), config: m.route }, m("i.arrow.right.icon"))
                             )
                         )
                     )
--- client/utils/dateFormatter.ts
+++ client/utils/dateFormatter.ts
@@ -2,21 +2,26 @@ import * as moment from "moment";
 
 export let formats = {
     urlFormat: "YYYY-MM-DD",
-    pickerFormat: "YYYY-MM-DD"
+    pickerFormat: "YYYY-MM-DD",
+    displayFormat: "dddd, MMMM Do YYYY"
 };
 
-export function formatDateForUrl(date: moment.Moment) {
+function formatDate(date: moment.Moment, format: string) {
     if (date == null || !date.isValid()) {
         return "";
     }
 
-    return date.format(formats.urlFormat);
+    return date.format(format);
+}
+
+export function formatDateForUrl(date: moment.Moment) {
+    return formatDate(date, formats.urlFormat);
 }
 
 export function formatDateForPicker(date: moment.Moment) {
-    if (date == null || !date.isValid()) {
-        return "";
-    }
+    return formatDate(date, formats.pickerFormat);
+}
 
-    return date.format(formats.pickerFormat);
+export function formatDateForDisplay(date: moment.Moment) {
+    return formatDate(date, formats.displayFormat);
 }
